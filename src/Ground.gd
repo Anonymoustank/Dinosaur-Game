3d89d7c0537bf687166c2e7921992e33b2cfae54
Anonymoustank <pkadekodi@gmail.com>
2021-12-24 14:32:12 -0600

    Randomly flips cacti horizontally now

--- src/Ground.gd
+++ src/Ground.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var base_cactus = preload("res://src/Small Cactus.tscn") #load small cactus
+var base_large_cactus = preload("res://src/Large Cactus.tscn") #load big cactus
 var new_enemy_time = 5 #elapsed time to load new enemy
+
+var rng = RandomNumberGenerator.new() #random number generator
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,8 +22,21 @@ func _process(delta: float) -> void:
 				new_enemy_time += (4 - Global.elapsed_time/25)
 			create_enemy()
 
+func random_num(minimum: int, maximum: int) -> int:
+	rng.randomize()
+	return rng.randi_range(minimum, maximum)
+
+func spawn_enemy(scene: PackedScene) -> void:
+	var new_cactus = scene.instance() #instantiate new cactus and put it right outside the screen
+	new_cactus.global_position = Vector2(1040, 560)
+	if random_num(1, 2) == 1:
+		new_cactus.set_scale(Vector2(-1,1))
+	add_child(new_cactus)
 
 func create_enemy() -> void:
-	var new_cactus = base_cactus.instance() #instantiate new cactus and put it right outside the screen
-	new_cactus.global_position = Vector2(1040, 560)
-	add_child(new_cactus)
+	var random_num = random_num(1, 2)
+	if random_num == 1: #spawn large cactus
+		spawn_enemy(base_large_cactus)
+	elif random_num == 2: #spawn small cactus
+		spawn_enemy(base_cactus)
+		
