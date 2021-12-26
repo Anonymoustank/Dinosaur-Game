f0efdf9c3cdefebe1419c8a6a0cffec5dea0ff04
Anonymoustank <pkadekodi@gmail.com>
2021-12-25 18:47:20 -0600

    Removed redundant small cactus/large cactus scripts and attached them to the enemy script instead

--- src/Ground.gd
+++ src/Ground.gd
@@ -10,19 +10,17 @@ var rng = RandomNumberGenerator.new() #random number generator
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !Global.dead and Global.started:
-		var num_enemies = get_tree().get_nodes_in_group("ENEMIES").size() #get all enemies (nodes that are in the ENEMIES group)
-		if Global.elapsed_time != 0 and Global.elapsed_time >= new_enemy_time and num_enemies < 2:
+		if Global.elapsed_time != 0 and Global.elapsed_time >= new_enemy_time: #spawn new cactus every set amount of time
 			if Global.elapsed_time >= 75:
 				new_enemy_time += 1.5
 			else:
 				new_enemy_time += (4 - Global.elapsed_time/25)
 			create_enemy()
 
-func random_num(minimum: int, maximum: int) -> int:
+func random_num(minimum: int, maximum: int) -> int: #returns random number between range (inclusive)
 	rng.randomize()
 	return rng.randi_range(minimum, maximum)
 
@@ -30,7 +28,7 @@ func spawn_enemy(scene: PackedScene) -> void:
 	var new_cactus = scene.instance() #instantiate new cactus and put it right outside the screen
 	new_cactus.global_position = Vector2(1040, 560)
 	if random_num(1, 2) == 1:
-		new_cactus.set_scale(Vector2(-1,1))
+		new_cactus.set_scale(Vector2(-1,1)) #flip orientation of cactus
 	add_child(new_cactus)
 
 func create_enemy() -> void:
